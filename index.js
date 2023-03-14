0a93a7a8e0f6825bf589d7adbe63a0461ece7a25
Lualbaldassari <lualbaldassari@gmail.com>
2023-03-14 10:24:20 -0500

    push

---
+++ index.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".menu-btn");
+const navlinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", function () {
+  navlinks.classList.toggle("mobile-menu");
+});
